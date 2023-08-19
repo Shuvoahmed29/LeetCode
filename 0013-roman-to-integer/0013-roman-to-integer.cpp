3c94fa88f45cd3c91b8252ce9758e45dd80583f4
Md.Raihanul Haque(Shuvo) <67226278+Shuvoahmed29@users.noreply.github.com>
2023-08-19 19:24:09 +0600

    Time: 3 ms (96.55%), Space: 6.1 MB (77.89%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int cost = 0;
+        for(int i=0;i<s.size();){
+           if(s[i]=='I' && s[i+1]=='V'){
+                   cost += 4;
+                   i = i+2;
+            }
+            else if(s[i]=='I' && s[i+1]=='X'){
+                   cost += 9;
+                   i = i+2;
+            }
+        
+           else if(s[i]=='X' && s[i+1]=='C'){
+           		cost += 90;
+           		i=i+2;
+           }
+           else if(s[i]=='X' && s[i+1]=='L'){
+           		cost += 40;
+           		i=i+2;
+           }
+          else if(s[i]=='C' && s[i+1]=='D'){
+          		cost += 400;
+          		i=i+2;
+          }
+          else if(s[i]=='C' && s[i+1]=='M'){
+          		cost += 900;
+          		i=i+2;
+          }
+          else{
+          	if(s[i]=='I') cost += 1;
+          	else if(s[i]=='V') cost += 5;
+          	else if(s[i]=='X') cost += 10;
+          	else if(s[i]=='L') cost += 50;
+          	else if(s[i]=='C') cost += 100;
+          	else if(s[i]=='D') cost += 500;
+          	else if(s[i]=='M') cost += 1000;
+          	i++;
+      	}    
+    }
+    return cost;
+    }
+};
