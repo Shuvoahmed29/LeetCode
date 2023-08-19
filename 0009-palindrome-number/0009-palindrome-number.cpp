f7fe9dfece955f8dbed02581b5be9b3b5c5b8569
Md.Raihanul Haque(Shuvo) <67226278+Shuvoahmed29@users.noreply.github.com>
2023-08-19 18:37:36 +0600

    Time: 11 ms (67.90%), Space: 6 MB (42.77%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string str = to_string(x);
+        string s = str;
+        reverse(str.begin(),str.end());
+        if(str==s) return true;
+        else return false;
+    }
+};
