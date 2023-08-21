635372b792aeabb322f56e55ae840a81ffc77b7f
Md.Raihanul Haque(Shuvo) <67226278+Shuvoahmed29@users.noreply.github.com>
2023-08-21 15:13:07 +0600

    Time: 0 ms (100.00%), Space: 7.7 MB (8.62%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
-    void help(vector<int>&vec){
-	sort(vec.begin(),vec.end());
-    reverse(vec.begin(),vec.end());
-	for(int i=0;i<vec.size()-1;i=0){
-		if(vec[i]!=vec[i+1]){
-			int p = vec[i]-vec[i+1];
-			vec.erase(vec.begin()+i+1);
-			vec.insert(vec.begin()+i+1,p);
-			vec.erase(vec.begin()+i);
-		}
-		else{
-			vec.erase(vec.begin()+i+1);
-			vec.erase(vec.begin()+i);
-            if(vec.size()==0) vec.insert(vec.begin(),0);
-		}
-		sort(vec.begin(),vec.end());
-    	reverse(vec.begin(),vec.end());
-        if(vec.size()==1) break;
-	}
-}
     int lastStoneWeight(vector<int>& vec) {
-        help(vec);
-        return vec[0];
+        priority_queue<int>Q;
+        for(auto it:vec) Q.push(it);
+        while(Q.size()>1){
+            int x=Q.top();
+            Q.pop();
+            int y=Q.top();
+            Q.pop();
+            if(x==y) continue;
+            else Q.push(x-y);
+        }
+        if(Q.size()==0) return 0;
+        else return Q.top();
     }
 };
